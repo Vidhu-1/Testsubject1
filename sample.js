3c5728fbb22e417077e87c03a8822e05f1efab3a
Vidhu-1 <vidhuvvsahari@gmail.com>
2020-02-17 09:52:55 +0530

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,2 +1,6 @@
 #Test subject 1
-var person = [{Name:"Baluvo", class:"10", age:"22", display:function(){console.log(this.Name)}},{Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}},{Name:"Dileep", class:"12", age:"20", display:function(){console.log(this.Name)}},{Name:"barjun", class:"1", age:"30", display:function(){console.log(this.Name)}},{Name:"vishnu", class:"2", age:"35", display:function(){console.log(this.Name)}}]
+var person = [{Name:"Baluvo", class:"10", age:"22", display:function(){console.log(this.Name)}},
+              {Name:"vichu", class:"10", age:"22", display:function(){console.log(this.Name)}},
+              {Name:"Dileep", class:"12", age:"20", display:function(){console.log(this.Name)}},
+              {Name:"barjun", class:"1", age:"30", display:function(){console.log(this.Name)}},
+              {Name:"vishnu", class:"2", age:"35", display:function(){console.log(this.Name)}}]
